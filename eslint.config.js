403ec6615d058a37fa216c1782711fdb03205293
ramiz4 <ramiz4@gmx.de>
2025-06-26 14:37:33 +0200

    chore: add react hooks plugin for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import typescript from "@typescript-eslint/eslint-plugin";
 import typescriptParser from "@typescript-eslint/parser";
 import importPlugin from "eslint-plugin-import";
 import unusedImports from "eslint-plugin-unused-imports";
+import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 
 export default [
@@ -28,9 +29,11 @@ export default [
       "@typescript-eslint": typescript,
       "unused-imports": unusedImports,
       import: importPlugin,
+      "react-hooks": reactHooks,
     },
     rules: {
       ...typescript.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
